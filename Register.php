9a4314bd95148bd9c2021636a80d3d875593e11f
Ishikawa Daichi <63411146+daichitt@users.noreply.github.com>
2020-04-17 00:12:25 +0900

    Add files via upload

---
+++ Register.php
@@ -0,0 +1,61 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+        <div class="container bg-warning">
+            <form action="../userAction.php" method="post">
+
+            <div class="card-header text-center mt-5">
+                 <h1>Register</h1>
+            </div>
+                <div class="header-body mt-5">
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-6">
+                                <label for="">First name</label>
+                                <input type="text" name="firstname" class="form-control" id="" placeholder="firstname">
+                            </div>
+                            <div class="col-6">
+                                <label for="">Last name</label>
+                                <input type="text" name="lastname" class="form-control" id="" placeholder="lastname">
+                            </div>
+                        </div>
+                        <br>
+                            <label for="" >Adress</label>
+                            <input type="text" name="adress" class="form-control" id="" placeholder="Adress">
+                            <br>
+                            <label for="" >Contact Number</label>
+                            <input type="text" name="countactnumber" class="form-control" id="" placeholder="countactnumber">
+                            <br>
+                            <label for="" >Email</label>
+                            <input type="text" name="email" class="form-control" id="" placeholder="Email">
+                            <br>
+                            <label for="">User name</label>
+                            <input type="text" name="username" class="form-control" id="" placeholder="username">
+                            <br>
+                            <label for="" >Password</label>
+                            <input type="text" name="password" class="form-control" id="" placeholder="password">
+                            <br>
+                            <button type="submit" name="register" class="col-6 btn btn-secondary btn-block">Submit</button>
+                            <br>
+                    </div>
+                </div>
+            </form>
+        </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ mainpage.php
@@ -0,0 +1,7 @@
+<?php include "header.php"?>
+
+<div class="container-fluid bg-dark">
+    <img src="../images/study.jpg" alt="" class="w-100 img-">
+</div>
+
+<?php include "footer.php"?>
